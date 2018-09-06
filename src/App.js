a56564876e15d69bf2da43dea3102f2bc4cae766
Michelle Lim <mlim@riskalyze.com>
2018-09-06 11:25:23 -0700

    Safety

--- src/App.js
+++ src/App.js
@@ -15,8 +15,72 @@ const myData = {
     }))
 };
 
+const testData = {
+  "nodes": [
+      {
+          "id": 0,
+          "name": "account_1",
+          "value": 10,
+          "color": "grey"
+      },
+      {
+          "id": 1,
+          "name": "client_10",
+          "value": 10,
+          "color": "grey"
+      },
+      {
+          "id": 2,
+          "name": "advisor_20",
+          "value": 15,
+          "color": "blue"
+      },
+      {
+          "id": 3,
+          "name": "account_2",
+          "value": 10,
+          "color": "grey"
+      }
+  ],
+  "links": [
+      {
+          "source": 1,
+          "target": 0
+      },
+      {
+          "source": 2,
+          "target": 1
+      },
+      {
+          "source": 1,
+          "target": 3
+      },
+      {
+          "source": 2,
+          "target": 1
+      }
+  ]
+}
+
+async function getData( ) {
+  await fetch('miserables.json', {
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  })
+    .then(function(res) {
+      console.log(res.text());
+    })
+    .catch(function(error) {
+      console.log(error.message);
+    });
+}
+
 class App extends Component {
   render() {
+    let data = getData();
+    console.log('data received: ' + data);
     return (
       <div className="App">
         <header className="App-header">
@@ -26,7 +90,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <ForceGraph3D graphData={myData}></ForceGraph3D>
+        <div className="force-graph-wrapper">
+          <ForceGraph3D graphData={testData}></ForceGraph3D>
+        </div>
       </div>
     );
   }
