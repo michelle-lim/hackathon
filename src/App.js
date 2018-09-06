bab25ca5f6f335b21b2853ba0cae53a17773cb10
Michelle Lim <mlim@riskalyze.com>
2018-09-06 10:24:15 -0700

    The force is with us. And not spitting errors.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import React, { Component } from 'react';
+// import ForceGraph3D from '3d-force-graph';
+import { ForceGraph2D, ForceGraph3D, ForceGraphVR } from 'react-force-graph';
 import logo from './logo.svg';
 import './App.css';
 
+// const N = 300;
+// const gData = {
+//   nodes: [...Array(N).keys()].map(i => ({ id: i })),
+//   links: [...Array(N).keys()]
+//     .filter(id => id)
+//     .map(id => ({
+//       source: id,
+//       target: Math.round(Math.random() * (id-1))
+//     }))
+// };
+
+// const Graph = ForceGraph3D()
+//       (document.getElementById('3d-graph'))
+//         .graphData(gData);
+
 class App extends Component {
   render() {
+    console.log(ForceGraph3D);
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Hello World!</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+
+
       </div>
     );
   }
